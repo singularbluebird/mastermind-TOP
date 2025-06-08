c588650593464914db6b53e250af309fd44f10ee
singularbluebird <samysarria.ss@gmail.com>
2025-06-08 19:04:52 -0300

    Added the code breaker class

---
+++ lib/CodeBreaker.rb
@@ -0,0 +1,45 @@
+class CodeBreaker
+  attr_accessor :name, :guess, :possible_guesses, :move_counter
+
+  def init(name='breaker')
+    @name = name
+    @move_counter = 0
+  end
+
+  def make_move()
+    @guess = gets.chomp.split('')
+    @move_counter +=1
+  end
+
+  def make_optimal_guess()
+    if possible_guesses.length == 1296
+      @guess = possible_guesses[7]
+    end
+  end
+
+# Method to change from number strings to plays
+  def make_play()
+    dict = {'0' => 'R', '1' => 'G', '2' => 'B', '3' => 'Y', '4' => 'O', 5 => 'P'} 
+    arr = []
+
+    (0..1295).each do |num|
+      arr << change_base(num, 6, 4).tr('012345', 'RGBYOP')
+    end
+    @possible_guesses = arr
+  end
+
+# Method to change base
+  def change_base(n, base, len)
+    if n == 0
+      return "0" * len
+    end
+
+    digits = []
+    while n > 0
+      digits.unshift(n % base)
+      n /= base
+    end
+
+    "0" * (len - (digits.length)) + digits.join 
+  end
+end
