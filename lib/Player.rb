5d43e17cd909bdd1ad6dcc4d8a2e572d13a2d9ed
singularbluebird <samysarria.ss@gmail.com>
2025-06-08 19:03:48 -0300

    Added the player class to avoid repetition.

---
+++ lib/Player.rb
@@ -0,0 +1,13 @@
+class Player
+  :name, :p_pos, :n_pos
+
+ def init(nam)
+   @name = nam
+   @n_pos = nil
+   @p_pos = nil
+ end 
+
+ # This is a stub method meant to be overwritten by descendant classes
+ def make_move()
+ end
+end
