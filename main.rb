587bdf979fee73fe87170efb54c784469b985e67
singularbluebird <samysarria.ss@gmail.com>
2025-06-10 20:52:41 -0300

    Makes it so main file runs the game with all the necessary classes

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
 require_relative 'lib/Game'
+require_relative 'lib/CodeMaker'
+require_relative 'lib/CodeBreaker'
+
+maker = CodeMaker.new
+breaker = CodeBreaker.new
+g = Game.new(maker, breaker)
+
+g.make_move
