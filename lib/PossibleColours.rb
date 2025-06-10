ac01b08b9d7ce126b02ef26351354ae496e24fc4
singularbluebird <samysarria.ss@gmail.com>
2025-06-10 19:36:15 -0300

    Added hashes of colours for players to use

--- lib/PossibleColours.rb
+++ lib/PossibleColours.rb
@@ -1,4 +1,6 @@
+require 'colorize'
+
 module PossibleColours
-  @possible_make_colours = 
-  @possible_guess_colours = 'RGBYOP'.split('')
+  @possible_make_colours = {'B': 9675.chr(Encoding::UTF_8), 'W': 9679.chr(Encoding::UTF_8)} 
+  @possible_guess_colours = {'R': 9679.chr(Encoding::UTF_8).colorize(:red), 'G': 9679.chr(Encoding::UTF_8).colorize(:green), 'B': 9679.chr(Encoding::UTF_8).colorize(:blue), 'Y': 9679.chr(Encoding::UTF_8).colorize(:yellow), 'O': 9679.chr(Encoding::UTF_8).colorize(:orange), 'P':9679.chr(Encoding::UTF_8).colorize(:pink) }
 end
