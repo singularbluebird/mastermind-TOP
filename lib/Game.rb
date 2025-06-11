9629249bb584667ba7c4e6dcecfdbd9966cde40f
singularbluebird <samysarria.ss@gmail.com>
2025-06-10 23:03:49 -0300

    Corrects colour functionality and checks for the kind of player in the next move

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,11 +1,10 @@
 require_relative 'CodeMaker'
 require_relative 'CodeBreaker'
 require_relative 'PossibleColours'
+require 'colorize'
 
 class Game
-  include PossibleColours
-
-  attr_accessor :board, :positions, :p_maker, :p_breaker, :previous_player, :next_player, :move_counter
+  attr_accessor :board, :positions, :p_maker, :p_breaker, :previous_player, :next_player, :move_counter, :possible_make_colours, :possible_guess_colours
 
   def initialize
 
@@ -15,6 +14,8 @@ class Game
     @previous_player = @p_maker
     @next_player = @p_breaker
     @move_counter = 0.0
+    @possible_make_colours = {'B'=> 9675.chr(Encoding::UTF_8), 'W'=> 9679.chr(Encoding::UTF_8)}
+    @possible_guess_colours = {'R'=> 9679.chr(Encoding::UTF_8).colorize(:red), 'G'=> 9679.chr(Encoding::UTF_8).colorize(:green), 'B'=> 9679.chr(Encoding::UTF_8).colorize(:blue), 'Y'=> 9679.chr(Encoding::UTF_8).colorize(:yellow), 'O'=> 9679.chr(Encoding::UTF_8).colorize(:orange), 'P'=>9679.chr(Encoding::UTF_8).colorize(:pink) }
     puts """Welcome to Mastermind. In this game, there is a code guesser and a code breaker.
     In here you'll play as the code breaker. The computer will pick a code of 4 possible colours and you have to guess it correctly. You get 12 guesses total. Each guess will appear in a row with the \"*\" on them and the feedback for that specific guess will be displayed besides it where the \"#\" is displayed."""
 
@@ -27,8 +28,14 @@ class Game
   def make_move()
     puts "#{@next_player.name} it is now your turn."
 
-    @next_player.make_move
+    if @next_player == @p_maker
+      @next_player.make_move(@previous_player.guess)
+    else
+      @next_player.make_move
+    end
+
     self.update_positions
+    p @positions
 
     if self.did_anyone_win?
       puts "Congratulations #{@next_player} is the winner, better luck next time #{@previous_player}"
@@ -44,10 +51,10 @@ class Game
   def update_positions
     if @next_player.instance_of? CodeBreaker
       self.positions[self.move_counter.floor].delete("#")
-      @next_player.guess.reverse.each { |g| self.positions.unshift(self.possible_guess_colours[g]) }
+      @next_player.guess.reverse.each { |g| self.positions[self.move_counter.floor].unshift(@possible_guess_colours[g]) }
     else
       self.positions[self.move_counter.floor].delete("*")
-      @next_player.feedback.reverse.each { |f| self.positions.unshift(self.possible_make_colours[f]) }
+      @next_player.feedback.reverse.each { |f| self.positions[self.move_counter.floor].unshift(@possible_make_colours[f]) }
     end
     @move_counter += 0.5
   end
